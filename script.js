bcb7f0a38577153e6bd0c498e64070af22416625
AxelPalWO <140984664+AxelPalWO@users.noreply.github.com>
2023-08-18 00:17:40 -0300

    portfolio creado

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const imagen = document.getElementById("twitter");
+const portfolio = document.getElementById("portfolio");
 
-document.addEventListener("mouseenter", function(){
-    if (imagen.src === ("./imágenes/580b57fcd9996e24bc43c53e.png")){
-        imagen.src = "./imágenes/Twitter-Logo-tumb-1280x720.png"
-    } else {
-        imagen.src = "./imágenes/580b57fcd9996e24bc43c53e.png"
-    }
-});
-document.addEventListener("mouseleave", function(){
+imagen.addEventListener("mousemove", function(){
     if (imagen.src === ("./imágenes/580b57fcd9996e24bc43c53e.png")){
         imagen.src = "./imágenes/580b57fcd9996e24bc43c53e.png"
     } else {
         imagen.src = "./imágenes/Twitter-Logo-tumb-1280x720.png"
     }
 });
+imagen.addEventListener("mouseout", function(){
+    if (imagen.src === ("./imágenes/580b57fcd9996e24bc43c53e.png")){
+        imagen.src = "./imágenes/Twitter-Logo-tumb-1280x720.png"
+    } else {
+        imagen.src = "./imágenes/580b57fcd9996e24bc43c53e.png"
+    }
+});
+portfolio.addEventListener("click",function(e){
+    window.location = "portfolio.html"
+})
